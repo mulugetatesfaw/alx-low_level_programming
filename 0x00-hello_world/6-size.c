22465282f8601aeb4aa94044d3103023735f113e
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-10-13 12:58:42 -0700

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main -entry
+ * return: always 0
+ */
+int main(void)
+{
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
+	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
+	return (0);
+}
+
+
+
+
+
+
+	
+
+	
+
+
+
+
