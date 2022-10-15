4fae8b0bc5052d150b1a17a58c7918da48e56212
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-10-15 11:37:54 -0700

    printing combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main- prints all possible combination of numbers
- * Return: Always 0
+ * main - print commbination of two single digit numbers
+ *
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ * Return: 0
  */
 int main(void)
 {
-	int num;
+	int c;
 
-	for (num = 0; num < 10; num++)
+	for (c = '0' ; c <= '9' ; c++)
 	{
-		putchar(num + '0');
-		if (num < 9)
+		putchar(c);
+		if (c != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
-	}
 }
