108e936ceca312640da8a0a31ce4e0ac0da835d2
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-10-15 08:18:33 -0700

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main -prints the alphabets in reverse order
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la,
+
+	for (la = 'z'; la >= 'a'; la--)
+		putchar(la);
+	putchar('\n');
+	return (0);
+}
