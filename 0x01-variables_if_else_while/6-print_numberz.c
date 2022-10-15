221ef96f8ad83289651103cfb31bc01811808cda
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-10-15 08:02:11 -0700

    print num in new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main -prints numbers from 0 t0 9 including new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
