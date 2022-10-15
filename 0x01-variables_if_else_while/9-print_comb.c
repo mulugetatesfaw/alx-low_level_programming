e2491878884a4c82f5ed1a80c05d3693c8133348
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-10-15 09:12:48 -0700

    number combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main- prints all possible combination of numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 58; num++)
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
