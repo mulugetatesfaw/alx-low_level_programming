ac48a4c0be9d1fe7aee66800654b5b5ef7d0af3d
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-10-18 08:06:03 -0700

    a program printing lower case letters in new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  print_alphabet - prints alphabets in new line
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef __HEADER1_H
 #define __HEADER1_H
 int _putchar(char c);
+void print_alphabet(void);
 #endif
