41291ab7865a0f709a054f1b1995470bae0e488d
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-10-18 08:51:41 -0700

    printing the lower case alphabets ten times in new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 - prints alphabets 10 times
+ *  returns: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
