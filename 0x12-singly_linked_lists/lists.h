06fb1802fd0d242550cf3252a44bf5b7db709e01
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-11-24 09:21:32 -0800

    a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x11-singly_linked_lists directory.
- */
-
+#ifndef LISTS
+#define LISTS
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *              for Holberton project
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -31,5 +25,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif 
