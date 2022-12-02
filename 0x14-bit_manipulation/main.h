2caf9fbbbab65537a66997efe6c233594ed65986
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2022-12-02 01:38:56 -0800

    function converting the string in binary into unsigned int

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -14,6 +11,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
 
-#endif 
+#endif
