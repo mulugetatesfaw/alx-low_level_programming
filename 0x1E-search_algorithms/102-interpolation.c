1240fd78fcfc8d2a2608e99b9c4e11264b956d7c
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-05-10 01:14:51 -0700

    interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int interpolation_search(int *arr, size_t size, int value) {
+    int low = 0, high = (size - 1);
+    while (low <= high && value >= arr[low] && value <= arr[high]) {
+        if (low == high) {
+            if (arr[low] == value) return low;
+            return -1;
+        }
+        int pos = low + (((double)(high - low) / (arr[high] - arr[low])) * (value - arr[low]));
+        if (arr[pos] == value) return pos;
+        if (arr[pos] < value) low = pos + 1;
+        else high = pos - 1;
+    }
+    return -1;
+}
